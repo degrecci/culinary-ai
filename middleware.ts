e09eaee73ae38c8f341708acdf877536b0228b58
degrecci <hdegrecci@gmail.com>
2023-06-14 21:59:13 -0300

    Signin / Signout routes with supabase

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,25 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
-// import type { Database } from "@/lib/database.types";
+import { Database } from "@/lib/supabase";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
+  const supabase = createMiddlewareClient<Database>({ req, res });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // if user is signed in and the current path is / redirect the user to /account
+  if (user && req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/recipes", req.url));
+  }
+
+  // if user is not signed in and the current path is not / redirect the user to /
+  if (!user && req.nextUrl.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   return res;
 }
