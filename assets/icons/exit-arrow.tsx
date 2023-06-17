a94917fb43e3a61796eeaa93ed02be7d85e4bb3e
degrecci <hdegrecci@gmail.com>
2023-06-16 23:09:54 -0300

    Add exit icon

---
+++ assets/icons/exit-arrow.tsx
@@ -0,0 +1,17 @@
+export const ExitIcon = () => (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="1.5"
+    className="w-5 h-5 ml-1"
+    viewBox="0 0 24 24"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+    />
+  </svg>
+);
