fc55afef12aa2f21655cc491440eff7b80a5e107
degrecci <hdegrecci@gmail.com>
2023-06-18 00:34:06 -0300

    Revert input component

--- app/signup/Form/index.tsx
+++ app/signup/Form/index.tsx
@@ -5,7 +5,6 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/app/components/Button";
 import { supabaseClient } from "@/services/supabase";
-import { Input } from "@/app/components/Input";
 
 const validationSchema = z.object({
   email: z.string().email("Invalid email"),
@@ -44,34 +43,42 @@ export const SignupForm = () => {
     router.refresh();
   };
 
+  const errorMessagesClasses = "text-xs text-red-600 mt-1";
+  const inputClasses =
+    "w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out";
+
   return (
     <form onSubmit={handleSubmit(handleSignUp)}>
       <div className="relative mb-4">
-        <Input
-          label="Name"
-          type="text"
-          name="username"
-          errorMessage={errors.username?.message}
-          register={register}
-        />{" "}
+        <label htmlFor="username" className="leading-7 text-sm text-gray-600">
+          Name
+        </label>
+        <input {...register("username")} type="text" className={inputClasses} />
+        {errors.username && (
+          <p className={errorMessagesClasses}>{errors.username?.message}</p>
+        )}
       </div>
       <div className="relative mb-4">
-        <Input
-          label="Email"
-          type="email"
-          name="email"
-          errorMessage={errors.email?.message}
-          register={register}
-        />
+        <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+          Email
+        </label>
+        <input {...register("email")} type="email" className={inputClasses} />
+        {errors.email && (
+          <p className={errorMessagesClasses}>{errors.email?.message}</p>
+        )}
       </div>
       <div className="relative mb-4">
-        <Input
-          label="Password"
+        <label htmlFor="username" className="leading-7 text-sm text-gray-600">
+          Password
+        </label>
+        <input
+          {...register("password")}
           type="password"
-          errorMessage={errors.password?.message}
-          name="password"
-          register={register}
+          className={inputClasses}
         />
+        {errors.password && (
+          <p className={errorMessagesClasses}>{errors.password?.message}</p>
+        )}
       </div>
       <Button type="submit">Sign up</Button>
     </form>
