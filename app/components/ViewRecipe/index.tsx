e2fe40ebdfd881723bbaf75c88e68ae71c3cd236
degrecci <hdegrecci@gmail.com>
2023-06-23 18:53:00 -0300

    Add space and text size styles

--- app/components/ViewRecipe/index.tsx
+++ app/components/ViewRecipe/index.tsx
@@ -6,35 +6,42 @@ type Props = {
   recipe: Recipe;
 };
 
-const ViewRecipe = ({ recipe }: Props) => {
-  function showJsonList(json: Json) {
-    if (!json) {
-      return "";
-    }
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-    return Object.entries(json).map(([key, values]) => (
-      <li key={key}>
-        <strong>{key}:</strong>
-        <ul>
-          {values.map((value: string, index: string) => (
-            <li key={index}>{value}</li>
-          ))}
-        </ul>
-      </li>
-    ));
+function showJsonList(json: Json) {
+  if (!json) {
+    return "";
   }
 
+  return Object.entries(json).map(([key, values]) => (
+    <li className="list-none mb-3" key={key}>
+      <strong>{capitalizeFirstLetter(key)}:</strong>
+      <ul>
+        {values.map((value: string, index: string) => (
+          <li key={index}>- {value}</li>
+        ))}
+      </ul>
+    </li>
+  ));
+}
+
+const ViewRecipe = ({ recipe }: Props) => {
   return (
     <div>
-      <h3 className="text-2xl font-bold">{recipe.title}</h3>
-      <p className="text-lg">{recipe.description}</p>
-      <p>{recipe.difficulty_level}</p>
-      <p>{recipe.prep_time}</p>
-      <p>{recipe.total_time}</p>
-      <p>{recipe.serves}</p>
-      <p>{showJsonList(recipe.ingredients)}</p>
-      <p>{showJsonList(recipe.instructions)}</p>
-      <p>{recipe.tips_and_variations}</p>
+      <h3 className="text-2xl font-bold mb-2">{recipe.title}</h3>
+      <p className="text-md mb-4">{recipe.description}</p>
+      <p className="text-sm">Difficult level: {recipe.difficulty_level}</p>
+      <p className="text-sm">Preparation time: {recipe.prep_time}</p>
+      <p className="text-sm">Total time: {recipe.total_time}</p>
+      <p className="text-sm mb-4">Serves: {recipe.serves}</p>
+      <p className="text-md mb-2 font-semibold">Ingredients</p>
+      <p className="text-sm mb-4">{showJsonList(recipe.ingredients)}</p>
+      <p className="text-md mb-2 font-semibold">Instructions</p>
+      <p className="text-sm mb-4">{showJsonList(recipe.instructions)}</p>
+      <p className="text-md font-semibold">Tips</p>
+      <p className="text-sm mb-8">{recipe.tips_and_variations}</p>
     </div>
   );
 };
