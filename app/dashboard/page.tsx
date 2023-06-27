f2dfce9d2e609e50ab3eacfd7b1fcaecbde8f24c
degrecci <hdegrecci@gmail.com>
2023-06-27 19:53:50 -0300

    Adjust recipes list and add created at info

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,10 @@ type Recipe = Database["public"]["Tables"]["recipes"]["Row"];
 
 export default async function Dashboard() {
   const supabase = createServerComponentClient({ cookies });
-  const { data } = await supabase.from("recipes").select();
+  const { data } = await supabase
+    .from("recipes")
+    .select("title, description, created_at")
+    .order("created_at", { ascending: false });
 
   const recipes = data as Recipe[];
 
@@ -18,25 +21,33 @@ export default async function Dashboard() {
       <div className="md:container md:mx-auto px-5 py-12">
         <RecipeModal />
         <div className="flex flex-wrap">
-          {recipes.map((recipe) => (
-            <div className="xl:w-1/4 md:w-1/2 p-4" key={recipe.id}>
-              <div className="bg-gray-100 p-6 rounded-lg">
-                <Image
-                  width={720}
-                  height={400}
-                  className="h-40 rounded w-full object-cover object-center mb-6"
-                  src="https://dummyimage.com/720x400"
-                  alt="content"
-                />
-                <h2 className="text-lg text-red-500 font-medium title-font mb-4">
-                  {recipe.title}
-                </h2>
-                <p className="leading-relaxed text-base">
-                  {recipe.description}
-                </p>
+          {recipes.map((recipe) => {
+            const date = new Date(recipe.created_at as string);
+            const formattedCreatedAt = date.toLocaleString();
+
+            return (
+              <div className="xl:w-1/4 md:w-1/2 p-4" key={recipe.id}>
+                <div className="bg-gray-100 p-6 rounded-lg">
+                  <Image
+                    width={720}
+                    height={400}
+                    className="h-40 rounded w-full object-cover object-center mb-6"
+                    src="https://dummyimage.com/720x400"
+                    alt="content"
+                  />
+                  <h2 className="text-lg text-red-500 font-medium title-font mb-4">
+                    {recipe.title}
+                  </h2>
+                  <p className="leading-relaxed text-base">
+                    {recipe.description}
+                  </p>
+                  <p className="text-xs text-gray-500 mt-3">
+                    {formattedCreatedAt}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </section>
