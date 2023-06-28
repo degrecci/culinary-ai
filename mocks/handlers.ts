0bfdf32d6720b4e9ef6d92edea803c1a938fa544
degrecci <hdegrecci@gmail.com>
2023-06-27 21:28:30 -0300

    Add random mock to recipes

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,4 +1,58 @@
 import { rest } from "msw";
+import { rand, randFood, randNumber } from "@ngneat/falso";
+
+function convertToString(obj: Record<string, unknown>): string {
+  return JSON.stringify(obj, null, 4);
+}
+
+const foodName = randFood();
+
+const randRecipe = {
+  description: `This classic ${foodName} recipe is perfect for any occasion. It's sure to be a crowd-pleaser.`,
+  difficulty_level: rand(["Easy", "Intermediate", "Hard"]),
+  ingredients: {
+    crust: [
+      "2 1/2 cups all-purpose flour",
+      "1 tsp salt",
+      "1 tsp sugar",
+      "1 cup cold unsalted butter, cut into small pieces",
+      "1/4 to 1/2 cup ice water",
+    ],
+    filling: [
+      "6 cups thinly sliced peeled Granny Smith apples",
+      "1/2 cup sugar",
+      "1/4 cup all-purpose flour",
+      "1 tsp ground cinnamon",
+      "1/4 tsp ground nutmeg",
+      "1/4 tsp salt",
+      "2 tbsp unsalted butter",
+    ],
+  },
+  instructions: {
+    crust: [
+      "In a large bowl, whisk together flour, salt, and sugar.",
+      "Using a pastry blender or your fingers, cut in butter until mixture resembles coarse crumbs.",
+      "Add ice water, 1 tablespoon at a time, until dough comes together.",
+      "Divide dough in half and shape into disks.",
+      "Wrap in plastic wrap and refrigerate for at least 30 minutes.",
+    ],
+    filling: [
+      "Preheat oven to 375°F.",
+      "In a large bowl, toss together apples, sugar, flour, cinnamon, nutmeg, and salt.",
+      "Pour filling into bottom crust and dot with butter.",
+      "Cover with top crust, seal edges, and cut slits in top to vent.",
+      "Bake for 45-50 minutes, until crust is golden brown and filling is bubbling.",
+    ],
+  },
+  prep_time: rand([5, 10, 20, 30, 60]),
+  serves: rand([1, 2, 4, 8, 12]),
+  tips_and_variations:
+    "For a decorative touch, use cookie cutters to cut shapes out of the top crust before baking.",
+  title: foodName,
+  total_time: rand([20, 30, 60, 120, 160]),
+};
+
+const content = convertToString(randRecipe);
 
 export const handlers = [
   rest.post("https://api.openai.com/v1/chat/completions", (req, res, ctx) => {
@@ -14,8 +68,7 @@ export const handlers = [
             index: 0,
             message: {
               role: "assistant",
-              content:
-                '{\n    "description": "This classic apple pie recipe is perfect for any occasion. With a flaky crust and sweet apple filling, it\'s sure to be a crowd-pleaser.",\n    "difficulty_level": "Intermediate",\n    "ingredients": {\n        "crust": [\n            "2 1/2 cups all-purpose flour",\n            "1 tsp salt",\n            "1 tsp sugar",\n            "1 cup cold unsalted butter, cut into small pieces",\n            "1/4 to 1/2 cup ice water"\n        ],\n        "filling": [\n            "6 cups thinly sliced peeled Granny Smith apples",\n            "1/2 cup sugar",\n            "1/4 cup all-purpose flour",\n            "1 tsp ground cinnamon",\n            "1/4 tsp ground nutmeg",\n            "1/4 tsp salt",\n            "2 tbsp unsalted butter"\n        ]\n    },\n    "instructions": {\n        "crust": [\n            "In a large bowl, whisk together flour, salt, and sugar.",\n            "Using a pastry blender or your fingers, cut in butter until mixture resembles coarse crumbs.",\n            "Add ice water, 1 tablespoon at a time, until dough comes together.",\n            "Divide dough in half and shape into disks.",\n            "Wrap in plastic wrap and refrigerate for at least 30 minutes."\n        ],\n        "filling": [\n            "Preheat oven to 375°F.",\n            "In a large bowl, toss together apples, sugar, flour, cinnamon, nutmeg, and salt.",\n            "Pour filling into bottom crust and dot with butter.",\n            "Cover with top crust, seal edges, and cut slits in top to vent.",\n            "Bake for 45-50 minutes, until crust is golden brown and filling is bubbling."\n        ]\n    },\n    "prep_time": 30,\n    "serves": 8,\n    "tips_and_variations": "For a decorative touch, use cookie cutters to cut shapes out of the top crust before baking.",\n    "title": "Classic Apple Pie",\n    "total_time": 75\n}',
+              content,
             },
             finish_reason: "stop",
           },
