3799882f23bb187aa224706598d0281d14fe25e1
degrecci <hdegrecci@gmail.com>
2023-06-29 21:02:15 -0300

    Remove food image from list

--- app/dashboard/components/List/index.tsx
+++ app/dashboard/components/List/index.tsx
@@ -91,13 +91,6 @@ export default function RecipesList({ serverRecipes }: ListProps) {
         return (
           <div className="xl:w-1/4 md:w-1/2 p-4" key={recipe.id}>
             <div className="bg-gray-100 p-6 rounded-lg">
-              <Image
-                width={720}
-                height={400}
-                className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/720x400"
-                alt="content"
-              />
               <h2 className="text-lg text-red-500 font-medium title-font mb-4">
                 {recipe.title}
               </h2>
--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dummyimage.com",
-        port: "",
-        pathname: "/720x400",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
