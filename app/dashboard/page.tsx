40840713beb4fd250979c7355051b090de7c1b15
degrecci <hdegrecci@gmail.com>
2023-06-30 18:23:46 -0300

    Request everything from recipes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,7 +14,7 @@ export default async function Dashboard() {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase
     .from("recipes")
-    .select("title, description, created_at, id")
+    .select("*")
     .order("created_at", { ascending: false });
 
   return (
