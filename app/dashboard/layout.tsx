15de7238423c86bdab7b0a3962b302724b67e2cb
degrecci <hdegrecci@gmail.com>
2023-07-04 20:34:23 -0300

    Change dashboard layout to use server

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,32 +1,20 @@
-"use client";
-import { supabaseClient } from "@/services/supabase";
 import { ExitIcon } from "@/assets/icons/exit-arrow";
 import { Navbar } from "../components/Navbar";
-import { useCallback, useEffect } from "react";
 import Link from "next/link";
-import { useUser } from "@/store/user";
+import { supabaseServer } from "@/services/server";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user, setUser } = useUser();
-  const getUser = useCallback(async () => {
-    const {
-      data: { user },
-    } = await supabaseClient.auth.getUser();
+  const {
+    data: { user },
+  } = await supabaseServer.auth.getUser();
 
-    if (!user) {
-      return (window.location.href = "/signin");
-    }
-
-    setUser({ email: user.email || "", id: user.id || "" });
-  }, []);
-
-  useEffect(() => {
-    getUser();
-  }, [getUser]);
+  if (!user) {
+    return (window.location.href = "/signin");
+  }
 
   return (
     <>
