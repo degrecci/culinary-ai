5b46200fa829c259ce9044cd2de56495d959f42c
Henrique Degrecci <hdegrecci@gmail.com>
2023-12-08 12:21:32 -0300

    Remove cypress comments

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,15 +1,5 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
+
 Cypress.Commands.add("signin", () => {
   cy.visit("http://localhost:3000/");
 
@@ -26,23 +16,11 @@ Cypress.Commands.add("signin", () => {
   cy.get('input[name="password"]').type(Cypress.env("user_password"));
   cy.get("#submit").click();
 });
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
+
 export {};
 
 declare global {
-  namespace Cypress {
+  declare namespace Cypress {
     interface Chainable {
       signin(): Chainable<void>;
     }
