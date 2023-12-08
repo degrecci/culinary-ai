2a8e57d730097af5344c701bd8a57bdadac321d4
Henrique Degrecci <hdegrecci@gmail.com>
2023-12-08 12:07:46 -0300

    Create signin command

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,19 +1,6 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
     // Start from the index page
-    cy.visit("http://localhost:3000/");
-
-    // Find a link with an href attribute containing "signin" and click it
-    cy.get('a[href*="signin"]').click();
-
-    // The new url should include "/signin"
-    cy.url().should("include", "/signin");
-
-    // The new page should contain an h1 with "Sign In page"
-    cy.get("h2").contains("Sign In");
-
-    cy.get('input[name="email"]').type(Cypress.env("user_email"));
-    cy.get('input[name="password"]').type(Cypress.env("user_password"));
-    cy.get("#submit").click();
+    cy.signin();
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,8 +10,22 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("signin", () => {
+  cy.visit("http://localhost:3000/");
+
+  // Find a link with an href attribute containing "signin" and click it
+  cy.get('a[href*="signin"]').click();
+
+  // The new url should include "/signin"
+  cy.url().should("include", "/signin");
+
+  // The new page should contain an h1 with "Sign In page"
+  cy.get("h2").contains("Sign In");
+
+  cy.get('input[name="email"]').type(Cypress.env("user_email"));
+  cy.get('input[name="password"]').type(Cypress.env("user_password"));
+  cy.get("#submit").click();
+});
 //
 //
 // -- This is a child command --
